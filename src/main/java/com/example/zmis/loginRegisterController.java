9bd547b45b2ba25f2363e1dedd211e0bbeabee9c
jelemiekun - pc <jeremieeden1946@gmail.com>
2024-04-10 15:09:50 +0800

    loginRegister ui done

--- src/main/java/com/example/zmis/loginRegisterController.java
+++ src/main/java/com/example/zmis/loginRegisterController.java
@@ -1,5 +1,56 @@
 package com.example.zmis;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class loginRegisterController {
+
+    @FXML
+    private AnchorPane anchorPaneOperation;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Button btnProceed;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    private PasswordField passwordFieldPassword;
+
+    @FXML
+    private TextField textFieldEmail;
+
+    @FXML
+    void btnLoginOnAction(TouchEvent event) {
+
+    }
+
+    @FXML
+    void btnProceedOnAction(TouchEvent event) {
+
+    }
+
+    @FXML
+    void btnRegisterOnAction(TouchEvent event) {
+
+    }
+
+    @FXML
+    void passwordFieldPasswordPressedEnter(KeyEvent event) {
+
+    }
+
+    @FXML
+    void textFieldEmailPressedEnter(KeyEvent event) {
+
+    }
 
 }
--- src/main/java/com/example/zmis/mon.java
+++
@@ -1,4 +0,0 @@
-package com.example.zmis;
-
-public class mon {
-}

1ea44febfab0682c121afc45dc6ca52eafd3b514
jelemiekun - laptop <jeremieeden1946@gmail.com>
2024-04-19 06:15:53 +0800

    updated setStyleSheet

--- src/main/java/com/example/zmis/mainController.java
+++ src/main/java/com/example/zmis/mainController.java
@@ -683,12 +683,12 @@ public class mainController implements Initializable {
 
     public static void setStyleSheets() {
         UserAgentBuilder.builder()
-                .themes(JavaFXThemes.MODENA) // Optional if you don't need JavaFX's default theme, still recommended though
-                .themes(MaterialFXStylesheets.forAssemble(true)) // Adds the MaterialFX's default theme. The boolean argument is to include legacy controls
-                .setDeploy(true) // Whether to deploy each theme's assets on a temporary dir on the disk
-                .setResolveAssets(true) // Whether to try resolving @import statements and resources urls
-                .build() // Assembles all the added themes into a single CSSFragment (very powerful class check its documentation)
-                .setGlobal(); // Finally, sets the produced stylesheet as the global User-Agent stylesheet
+                .themes(JavaFXThemes.MODENA)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
     }
 
     private void openFromDashboardFXML() {
